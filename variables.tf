1ef64a805a2b4ee355001e3daab1ebdeb0628600
Mohammed Yahaya <daudaauwal@yahoo.co.uk>
2025-01-04 02:01:57 -0500

    change date again

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   type        = string
   description = "(Optional) Name of resource group to create. Defaults to oidc-test."
-  default     = "oidc-simple-20250104"
+  default     = "oidc-simple-20250105"
 }
 
 variable "location" {
